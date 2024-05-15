1ed90e9d433e1378770e7915c5743a2dce992e48
Johand <johand@aptika.co.id>
2024-05-15 12:20:12 +0700

    finish

--- models/index.js
+++ models/index.js
@@ -1,14 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-const userSchema = new mongoose.Schema({
-    userName: { type: String, required: true },
-    accountNumber: { type: Number, required: true, unique: true },
-    emailAddress: { type: String, required: true, unique: true },
-    identityNumber: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+const userSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: true
+        },
+        accountNumber: {
+            type: Number,
+            required: true,
+            unique: true
+        }
+        ,
+        emailAddress: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        identityNumber: {
+            type: String,
+            required: true,
+            unique: true,
+            index : -1
+        },
+        password: {
+            type: String,
+            required: true,
 
-});
+        },
+    }
+);
+
+userSchema.index({ identityNumber: -1 })
 
 const userData = new Schema(userSchema, { collection: "user_data" })
 const UserData = mongoose.model('User_data', userData)
